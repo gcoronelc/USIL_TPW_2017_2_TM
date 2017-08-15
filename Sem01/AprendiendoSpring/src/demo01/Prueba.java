85488b750060ad5cc3f7cb99d0c6774e037c9a8b
Gustavo Coronel <gcoronelc@gmail.com>
2017-08-15 09:45:38 -0500

    Sem 01

---
+++ Sem01/AprendiendoSpring/src/demo01/Prueba.java
@@ -0,0 +1,28 @@
+package demo01;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class Prueba {
+
+  public static void main(String[] args) {
+
+    // Instanciando el contexto
+    String contexto = "/demo01/contexto.xml";
+    BeanFactory beanFactory;
+    beanFactory = new ClassPathXmlApplicationContext(contexto);
+
+    // Accediendo a un bean
+    MateService service;
+    service = (MateService) beanFactory.getBean("mate");
+    
+    // Probando el bean
+    System.out.println("5 + 1 = " + service.sumar(5, 1));
+    
+  }
+
+}
