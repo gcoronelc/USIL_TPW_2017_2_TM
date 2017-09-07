1fa9b1ad11bc64476dcbee16ad4347f52f5a26f1
Gustavo Coronel <gcoronelc@gmail.com>
2017-09-07 09:45:27 -0500

    Sem 04

---
+++ Sem04/DemoMVC/src/pe/egcc/eureka/controller/AppController.java
@@ -0,0 +1,14 @@
+package pe.egcc.eureka.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppController {
+
+	@RequestMapping(value="index.htm", method=RequestMethod.GET)
+	public String index(){
+		return "index";
+	}
+}
